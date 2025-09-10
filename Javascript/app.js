d940a03c4656c41c3fc21556095e47a62acaaab3
PranabeshBasu <pranabeshbasu9@gmail.com>
2025-09-10 13:27:24 +0530

    Updated format

---
+++ Javascript/app.js
@@ -0,0 +1,6 @@
+console.log("Hello World!!!");
+//Usage of template literals in JS
+let pencilPrice = 10;
+let penPrice = 15;
+let output = `The total price is ${pencilPrice+penPrice} Rupees`;
+console.log(output)
