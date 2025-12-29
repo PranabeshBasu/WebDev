301b25cb7c39514a0e11159ebb87f05b4ded6c1e
PranabeshBasu <pranabeshbasu9@gmail.com>
2025-12-29 20:28:36 +0530

    29th Dec

---
+++ Javascript/Basics/Arrays/prob2.js
@@ -0,0 +1,3 @@
+let n = 3;
+let arr = [7,9,0,-2];
+console.log(arr.slice(0,n));
---
+++ Javascript/Basics/Arrays/prob4.js
@@ -0,0 +1,8 @@
+let str = prompt("please enter a string");
+
+if(str.length == 0){
+    console.log("The string is empty");
+}
+else{
+    console.log("The string is not empty");
+}
