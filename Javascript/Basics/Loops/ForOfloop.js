99196080bfa12d60537774d8e992916849eb46f6
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-04 15:18:44 +0530

    For Of Loop

---
+++ Javascript/Basics/Loops/ForOfloop.js
@@ -0,0 +1,8 @@
+fruits = ["Mango","Apple","Banana","Orange"]
+for(fruit of fruits){
+    console.log(fruit); // This fruit variable will store the array elements one by one
+    // no counter variable needed
+}
+for(char of "PranabeshBasu"){
+    console.log(char);
+}
---
+++ Javascript/Basics/Loops/Nestedforof.js
@@ -0,0 +1,7 @@
+let sdata = [["Pranabesh", 'C',19],["Prantika Ghosh",'C',20],["Pritam Karak",'C',22]];
+for(students of sdata){
+    console.log(students);
+    for(data of students){
+        console.log(data);
+    }
+}
