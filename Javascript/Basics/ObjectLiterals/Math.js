012916dbd5329f666252644a064101f7c0eb0c1f
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-09 12:15:12 +0530

    Math Object

---
+++ Javascript/Basics/ObjectLiterals/Math.js
@@ -0,0 +1,8 @@
+console.log(Math.PI);
+console.log(Math.E);
+
+console.log(Math.abs(-6));
+console.log(Math.pow(2,3));
+console.log(Math.floor(5.0000000001));
+console.log(Math.ceil(5.9999999999));
+console.log(Math.random());
