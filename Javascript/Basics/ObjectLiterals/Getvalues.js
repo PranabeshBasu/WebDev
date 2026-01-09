c1a9226dacb60bd656bfb646e6f930360c5a15ba
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-09 11:32:45 +0530

    Keys as Strings

---
+++ Javascript/Basics/ObjectLiterals/Getvalues.js
@@ -0,0 +1,13 @@
+const Post = {
+    username : "_pranabeshh",
+    post : "This is my first post",
+    likes : 256,
+    reposts : 12,
+    tags : ["pritamakarak", "rajbirdey_official"]
+};
+console.log(Post);
+console.log(Post["post"]);
+console.log(Post.likes);
+let tagged_accs = "tags";
+console.log(Post[tagged_accs]);
+console.log(Post.tagged_accs);
---
+++ Javascript/Basics/ObjectLiterals/KeysAsStrings.js
@@ -0,0 +1,7 @@
+const Keys = {
+    1 : 'a',
+    2 : 'b',
+    true : 'c',
+    undefined : 'd'
+};
+console.log(Keys);
