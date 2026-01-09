d9cb2fee76f288d27427aeaa924621dbf9fe5a89
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-09 12:03:22 +0530

    Array of objects

---
+++ Javascript/Basics/ObjectLiterals/Arrayofobjs.js
@@ -0,0 +1,26 @@
+let studentinfo = [
+    {
+        name : "Pranabesh Basu",
+        year : "3rd",
+        semester : "6th",
+        sec : 'c',
+        roll : 19
+    },
+    {
+        name : "Prtam Karak",
+        year : "3rd",
+        semester : "6th",
+        sec : 'c',
+        roll : 22
+    },
+    {
+        name : "Rajbir Dey",
+        year : "3rd",
+        semester : "6th",
+        sec : 'c',
+        roll : 30 
+    }
+]
+console.log(studentinfo);
+console.log(studentinfo[1]);
+console.log(studentinfo[1].semester);
