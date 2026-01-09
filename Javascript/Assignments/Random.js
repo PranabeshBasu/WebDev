1b982aea118f61b3cec1601e242f10fd25b4fa49
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-09 12:26:57 +0530

    generating random nos

---
+++ Javascript/Assignments/Random.js
@@ -0,0 +1,4 @@
+let random1 = Math.floor( Math.random() * 100 ) + 1; // Random nos from 1 to 100
+console.log(random1);
+let random2 = Math.floor( Math.random() * 5 ) + 1; //Random nos from 1 to 5
+console.log(random2);
