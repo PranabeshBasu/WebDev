5728831ee7ecef4556155be082912b9981fe228d
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-14 01:46:52 +0530

    Basic qs

---
+++ Javascript/Basics/Functions/Average.js
@@ -0,0 +1,5 @@
+function average(num1, num2, num3) {
+    let avg = (num1+num2+num3)/3;
+    console.log(avg);
+}
+average(2,3,4);
---
+++ Javascript/Basics/Functions/mult.js
@@ -0,0 +1,6 @@
+function multTable(num) {
+    for(let i = 1; i <= 10; i++) {
+        console.log(`${num} * ${i} = ${num*i}`);
+    }
+}
+multTable(5);
