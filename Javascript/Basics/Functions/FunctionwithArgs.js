1851c4479b133e33a13c61a0d5649c0ad12a4fb7
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-14 01:37:41 +0530

    Function with Arguments

---
+++ Javascript/Basics/Functions/FunctionwithArgs.js
@@ -0,0 +1,10 @@
+function personalInfo(name, age) {
+    console.log(`${name}'s age is ${age}`)
+}
+personalInfo("Pranabesh",21); 
+
+//sum of two numbers
+function sum(a,b) {
+    console.log(a+b);
+}
+sum(9,10);
