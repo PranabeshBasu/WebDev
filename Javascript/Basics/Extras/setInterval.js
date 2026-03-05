9189e66d42ee64a51fdb293492a6695ea711bebc
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-03-05 18:08:28 +0530

    setInterval function

---
+++ Javascript/Basics/Extras/setInterval.js
@@ -0,0 +1,7 @@
+let id1 = setInterval(() => {
+    console.log("Welcome to APNA COLLEGE");
+},2000);
+let id2 = setInterval(() => {
+    console.log("We will learn developmemt");
+},2000);
+clearInterval(id1);
